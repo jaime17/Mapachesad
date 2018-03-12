840f9365abc7746e12ae472b5e83a7833bb340e7
jaime17 <A01411527@itesm.mx>
2018-03-11 20:27:29 -0600

    Initial commit

---
+++ mapachesad/Assets/Sprites/bureaucrat/SadDino.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SadDino : MonoBehaviour {
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Vector2 jumpForce;
+    public float maxVel = 5f;
+    public float yjumpForce = 300f;
+    private bool isJumping = false;
+    private bool movingRight = true;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        jumpForce = new Vector2(0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float v = Input.GetAxis("Horizontal"); //aactualizacion de velocidad horizontal;
+        Vector2 vel = new Vector2(0, rb.velocity.y);
+        v *= maxVel;
+        vel.x = v;
+        rb.velocity = vel;
+
+        if (v != 0)
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+
+        if(Input.GetAxis("Jump") > 0.01f)
+        {
+            if (!isJumping)
+            {
+                if (rb.velocity.y == 0f)
+                {
+                    isJumping = true;
+                    jumpForce.x = 0f;
+                    jumpForce.y = yjumpForce;
+                    rb.AddForce(jumpForce);
+
+                }
+            }
+        }
+        else
+        {
+            isJumping = false;
+        }
+        if (movingRight && v < 0)
+        {
+            movingRight = false;
+            Flip();
+        }else if (!movingRight && v > 0)
+        {
+            movingRight = true;
+            Flip();
+        }
+	}
+
+    private void Flip()
+    {
+        var s = transform.localScale;
+        s.x = -1;
+        transform.localScale = s;
+    }
+}
---
+++ mapachesad/Assets/Sprites/scripts/controladorpersonaje.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controladorpersonaje : MonoBehaviour {
+    public float jump =100f;
+    private Animator animator;
+    private bool suelo = true;
+    public Transform comsalto;
+    private float radio = 0.07f;
+    public LayerMask mascaraSuelo;
+    private bool dobleSalto = false;
+   
+    public float separacion =3f;
+    public float speed = 15.0f;
+    
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+   void FixedUpdate()
+    {
+        suelo = Physics2D.OverlapCircle(comsalto.position, radio, mascaraSuelo);
+        animator.SetBool("isground",suelo);
+        if (suelo)
+        {
+            dobleSalto = false;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetKey(KeyCode.RightArrow))
+        {
+            
+            transform.position += Vector3.right * speed * Time.deltaTime;
+            animator.SetBool("iswalk", true);
+        }
+           else 
+        {
+            animator.SetBool("iswalk", false);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime ;
+            animator.SetBool("iswalk", true);
+            flip();
+            
+        }
+        else
+        {
+            animator.SetBool("iswalk", false);
+            
+        }
+
+        if ((suelo||!dobleSalto) && Input.GetKeyUp(KeyCode.UpArrow))
+        {
+          
+            transform.position += Vector3.up*jump * Time.deltaTime;
+            
+            if (!dobleSalto && !suelo)
+            {
+                dobleSalto = true;
+            }
+        }
+
+       
+    }
+    void flip()
+    {
+        var s = transform.localScale;
+        s.x *= -1;
+        transform.localScale = s;
+    }
+    
+}
